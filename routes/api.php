129eedffc928414b28ebe97cdda7c1e7cbe31285
jvitormichels <jvitormichels@gmail.com>
2025-06-14 16:51:49 -0300

    feat: autenticação básica com Sanctum Problema: requisições que deveriam retornar Unauthenticated estão roteando para /login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\UserAuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [UserAuthController::class, 'register']);
+    Route::post('login', [UserAuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('logout', [UserAuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () { Route::get('/user', [UserAuthController::class, 'show']); });
+});
