e68f1c085c9cbe5c855480f9964ca84fe77ba8f9
jvitormichels <jvitormichels@gmail.com>
2025-06-16 23:40:42 -0300

    feat: listagem de comentário por post e criação de comentário

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function store(Request $request, Post $post)
+    {
+        $validated = $request->validate([
+            'body' => 'required|string|max:2000',
+        ]);
+
+        $comment = $post->comments()->create([
+            'body' => $validated['body'],
+            'user_id' => auth()->id(),
+            'post_id' => $post->id
+        ]);
+
+        return response()->json([
+            'message' => 'Comment added successfully',
+            'comment' => $comment->load('user')
+        ], 201);
+    }
+
+    public function index(Post $post)
+    {
+        $comments = $post->comments()->with(['user']);
+
+        return response()->json($comments);
+    }
+}
