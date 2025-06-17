d5b37eae1c00caa7ec7f6a2fbe5ba882082e04b0
jvitormichels <jvitormichels@gmail.com>
2025-06-16 23:39:32 -0300

    fix: reorganização de rotas

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\{
     UserAuthController,
     PostsController,
     FollowsController,
-    UsersController
+    UsersController,
+    CommentsController
 };
 
 
@@ -18,15 +19,7 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth:sanctum')->group(function () { Route::get('/user', [UserAuthController::class, 'show']); });
 });
 
-Route::prefix('posts')->group(function () {
-    Route::get('/{post}', [PostsController::class, 'show']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/', [PostsController::class, 'timeline']);
-        Route::post('/', [PostsController::class, 'store']);
-        Route::delete('/{post}', [PostsController::class, 'destroy']);
-    });
-});
 
 Route::prefix('users')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
@@ -37,9 +30,23 @@ Route::prefix('users')->group(function () {
 Route::prefix('users/{user}')->namespace('App\Http\Controllers')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/posts', [PostsController::class, 'userPosts']);
+
         Route::get('/followers', [FollowsController::class, 'followers']);
         Route::get('/following', [FollowsController::class, 'following']);
         Route::post('/follow', [FollowsController::class, 'store']);
         Route::delete('/follow', [FollowsController::class, 'destroy']);
+    });
+});
+
+
+Route::prefix('posts')->group(function () {
+    Route::get('/{post}', [PostsController::class, 'show']);
+    Route::get('/{post}/comments', [CommentsController::class, 'index']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/', [PostsController::class, 'timeline']);
+        Route::post('/', [PostsController::class, 'store']);
+        Route::delete('/{post}', [PostsController::class, 'destroy']);
+        
+        Route::post('/{post}/comments', [CommentsController::class, 'store']);
     });
 });
